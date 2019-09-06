e9749b148b0b118125f6ddd20d9cf33ef53c17c4
tgno3 <tgno3.com@gmail.com>
2019-09-07 08:39:36 +0900

    Table 1 & 2 added

--- FlexBoost on GIT.R
+++ FlexBoost on GIT.R
@@ -27,7 +27,6 @@ independent <- 1:3
 dependent   <- 4
 s           <- 9 #Seed
 
-
 #########################################################################################################################
 ### Analysis
 #########################################################################################################################
@@ -41,7 +40,7 @@ y.2 <- exp(-s*x)
 y.3 <- exp(-(1/s)*x)
 m   <- min(y.1, y.2, y.3)
 M   <- max(y.1, y.2, y.3)
-plot(x, y.1, type = "l", col = "black", ylim = c(m, M), xlab = "y??f", ylab = "LOSS", lwd = l)
+plot(x, y.1, type = "l", col = "black", ylim = c(m, M), xlab = "y¡¤f", ylab = "LOSS", lwd = l)
 grid()
 par(new = TRUE)
 plot(x, y.2, type = "l", col = "red",   ylim = c(m, M), xlab = "", ylab = "", lwd = l)
@@ -53,19 +52,21 @@ segments(-0, 0, 1, 0, lty = l, col = "orange", lwd = l)
 legend(0, 5.2, legend = c("Exponential K > 1", "Exponential K = 1", "Exponential K < 1", "Classification\n(Zero-One)"),
        col = c("red", "black", "blue", "orange"), lty = c(1, 1, 1, 2), cex = 0.74, lwd = l)
 
-
-# Table 1. Description of the datasets
-table.1 <- matrix(NA, length(df.all), 2, dimnames = list(names(df.all), c("No.Instances", "No.Attributes")))
-for(i in 1:nrow(table.1)){table.1[i,] <- dim(df.all[[i]]) - c(0, sum(grepl("class", names(df.all[[i]]))) - 1)}
-print(table.1[-c(17:18, 20:21, 23:24, 26:27, 29:30),])
-
-
 ## Figure 3. Performance benchmarks on 30 UCI and Kaggle datasets
 plot.result(29) # Plot 1-29 data accuracies(Except Iris_1 due to acc 1)
 
 
+## Table 1. Description of the datasets
+table.1 <- matrix(NA, length(df.all), 2, dimnames = list(names(df.all), c("No.Instances", "No.Attributes")))
+for(i in 1:nrow(table.1)){table.1[i,] <- dim(df.all[[i]]) - c(0, sum(grepl("class", names(df.all[[i]]))) - 1)}
+print(table.1[-c(17:18, 20:21, 23:24, 26:27, 29:30),])
+
 ## Table 2. Performance benchmarks (accuracy)
-Table.2
+res.r1  <- subset(res.acc.all, Round == 9)
+acc.r1  <- rbind(res.r1[,3:6], colMeans(res.r1[,c(3:6)]))
+table.2 <- data.frame(Data = c(as.character(res.r1$Data), "Avg.Acc"),
+                      round(acc.r1, 6), Optimal.K = c(res.r1$Optimal.K, NA))
+print(table.2)
 
 ## Table 3. Friedman post hoc test results (p-value) on rankings and average accuracies
 # Load acc step_1
